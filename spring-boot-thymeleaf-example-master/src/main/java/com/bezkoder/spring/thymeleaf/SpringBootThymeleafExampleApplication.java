78a9478c146ba9304540d77ae5545f9279ad1df9
DESKTOP-P5PQ5EQ\Precise <softwaree831@gmail.com>
2024-05-07 17:10:35 +0530

    change new codes

--- spring-boot-thymeleaf-example-master/src/main/java/com/bezkoder/spring/thymeleaf/SpringBootThymeleafExampleApplication.java
+++ spring-boot-thymeleaf-example-master/src/main/java/com/bezkoder/spring/thymeleaf/SpringBootThymeleafExampleApplication.java
@@ -1,13 +1,21 @@
 package com.bezkoder.spring.thymeleaf;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootThymeleafExampleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
+		
+		 
 		SpringApplication.run(SpringBootThymeleafExampleApplication.class, args);
+		 
+		  InetAddress localHost = InetAddress.getLocalHost();
+		  System.out.println("LOCALHOST :"+localHost);
 	}
 
 }
--- spring-boot-thymeleaf-example-master/src/main/resources/static/ajax/product.js
+++ spring-boot-thymeleaf-example-master/src/main/resources/static/ajax/product.js
@@ -1,41 +1,82 @@
 $(document).ready(function(e) {
-alert("Product Page Is Loaded .. ");
-			$.ajax({
-				url: 'http://localhost:8000/api/category/list/active',
-				type: 'GET',
-				dataType: 'json',
-				contentType: "application/json",
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				success: function(
-					data,
-					textStatus,
-					xhr) {
-					console.log(data);
-						arr = data["body"];
-		console.log("array is :"+arr);
+	//==== category is load page loading time 
+ 
+	$.ajax({
+		url: 'http://localhost:8000/api/category/list/active',
+		type: 'GET',
+		dataType: 'json',
+		contentType: "application/json",
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		success: function(
+			data,
+			textStatus,
+			xhr) {
+			arr = data["body"];
 			arr.forEach(myFunction);
-				},
-				error: function(
-					xhr,
-					textStatus,
-					errorThrown) {
+		},
+		error: function(
+			xhr,
+			textStatus,
+			errorThrown) {
 			console.log(xhr);
-				}
-			});
-		
+		}
+	});
+
+
+
+	//========= submittion product ==============
+
+	$('form').submit(function(e) {
+		var productname = $('.productname').val();
+		var stock = $('.stock').val();
+		var category = $('.category').val();
+		var serialnumber = $('.serialnumber').val();
+		var size = $('.size').val();
+		// ajax call start
+		$.ajax({
+			url: 'http://localhost:8000/api/product/saved',
+			type: 'POST',
+			dataType: 'json',
+			contentType: "application/json",
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			data: JSON.stringify({
+				"productname": productname,
+				"stocks": stock,
+				"serialnumber": serialnumber,
+				"categoryid": category,
+				"size": size,	
+				"price":"800"
+			}),
+			success: function(
+				data,
+				textStatus,
+				xhr) {
+				alert("SUCCESS");
+			},
+			error: function(
+				xhr,
+				textStatus,
+				errorThrown) {
+				alert("FAILLED");
+			}
+		});
+
+
+
+
+		//ajax calll end 
+	});
+
 });
-
 function myFunction(v, i, a) {
-	
-	console.log(v)
-	console.log(i); 
-	console.log(a);
 	$(".addvaluselectoption")
-		.append("<option value='"+v.categoryid+"'>" + v.categoryname + "</option> ");
-
+		.append("<option value='" + v.categoryid + "'>" + v.categoryname + "</option> ");
 }
 
 
